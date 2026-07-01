6fe1ef01aab2352a4c80c6223c78b3fbfe3a4051
ahtesham <ahmadahtesham0935@gmail.com>
2026-07-01 14:22:08 +0530

    clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,18 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://res.cloudinary.com" />
         <link rel="dns-prefetch" href="https://img.clerk.com" />
         <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xfjpcfhfxu");
+            `
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify([organizationJsonLd(), websiteJsonLd(), softwareJsonLd()])
